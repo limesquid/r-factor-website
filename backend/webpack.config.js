6f766df64815699120fe61515649116a497b8dc7
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-22 02:28:09 +0200

    Passing BIN_PATH through package.json

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -52,9 +52,10 @@ module.exports = {
     IS_DEV_ENV && new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         API_HOST: JSON.stringify(process.env.API_HOST),
-        API_PORT: JSON.stringify(process.env.API_PORT)
+        API_PORT: JSON.stringify(process.env.API_PORT),
+        BIN_PATH: JSON.stringify(process.env.BIN_PATH),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     })
   ].filter(Boolean)
