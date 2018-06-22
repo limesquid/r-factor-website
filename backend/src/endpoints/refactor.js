9c3669f17f7eac7cd2346e1a3c152464b6508af8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-22 02:24:41 +0200

    Refactor endpoint

--- backend/src/endpoints/refactor.js
+++ backend/src/endpoints/refactor.js
@@ -1,9 +1,67 @@
+import path from 'path';
+import { spawn } from 'child_process';
+
+const BIN_PATH = path.join('C:', 'Users', 'Kamil', 'AppData', 'Roaming', 'Sublime Text 3', 'Packages', 'r-factor', 'dist', 'index.js');
+const MAX_CODE_LENGTH = 4000;
+const TIMEOUT = 30 * 1000;
+const TIMEOUT_MESSAGE = `Refactoring timed out (${TIMEOUT / 1000}s)`;
+const UNKNOWN_REFACTORING_MESSAGE = 'Unknown refactoring method';
+const TOO_LONG_INPUT_MESSAGE = `You cannot refactor more than ${MAX_CODE_LENGTH} characters here. Buy the plugin.`;
+const REFACTORINGS = [
+  'add-classname',
+  'convert-to-class-component',
+  'convert-to-functional-component',
+  'generate-prop-types',
+  'move-default-props-out-of-class',
+  'move-default-props-to-class',
+  'move-prop-types-out-of-class',
+  'move-prop-types-to-class',
+  'sort-attributes',
+  'sort-imports',
+  'connect',
+  'connect-with-map-dispatch',
+  'connect-with-map-state',
+  'disconnect',
+  'disconnect-map-state',
+  'disconnect-map-dispatch'
+];
+
 export default (request, response) => {
-  const { code, refactoring } = parseRequest(request);
-  response.send(code);
+  const { code, refactoring, settings } = parseRequest(request);
+
+  if (!REFACTORINGS.includes(refactoring)) {
+    return response.status(400).send(UNKNOWN_REFACTORING_MESSAGE);
+  }
+
+  if (code.length > MAX_CODE_LENGTH) {
+    return response.status(400).send(TOO_LONG_INPUT_MESSAGE);
+  }
+
+  let stdout = '';
+  let stderr = '';
+  const child = spawn('node', [ BIN_PATH, '-r', refactoring, '-s', settings ]);
+  const timeout = setTimeout(() => {
+    response.status(500).send(TIMEOUT_MESSAGE);
+    child.kill();
+  }, TIMEOUT);
+
+  child.stdin.setEncoding('utf-8');
+  child.stdin.write(code);
+  child.stdin.end();
+  child.stdout.on('data', (data) => { stdout += data.toString(); });
+  child.stderr.on('data', (data) => { stderr += data.toString(); });
+  child.on('close', () => {
+    clearTimeout(timeout);
+    if (stderr) {
+      response.status(404).send(stderr);
+    } else {
+      response.send(stdout);
+    }
+  });
 };
 
 const parseRequest = ({ body }) => ({
   code: body.code,
-  refactoring: body.refactoring
+  refactoring: body.refactoring,
+  settings: JSON.stringify(body.settings || {})
 });
