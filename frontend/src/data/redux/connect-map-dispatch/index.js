3ec5a4fcfd3bb002483c1c3a13f2306db89f8dc6
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-27 00:49:10 +0200

    Refactoring redux data

---
+++ frontend/src/data/redux/connect-map-dispatch/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default {
+  id: 'connect-map-dispatch',
+  name: 'Connect component (mapDispatchToProps)',
+  description: (
+    <div>
+      Connects a component to redux store and generates <code>mapDispatchToProps</code> only.
+    </div>
+  )
+};
---
+++ frontend/src/data/redux/disconnect-map-state/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default {
+  id: 'disconnect-map-state',
+  name: 'Disconnect component (mapStateToProps)',
+  description: (
+    <div>
+      Disconnects <code>mapStateToProps</code> from a component connected to a redux store.
+      Disconnects completely if possible.
+    </div>
+  )
+};
