1c6bef0b6bbce3faa6309c9a966de01110caf648
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-27 02:08:12 +0200

    Add classname docs

---
+++ frontend/src/data/react/add-classname/example2.js
@@ -0,0 +1,39 @@
+const input = `import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="button">
+        Button
+      </div>
+    );
+  }
+}
+
+export default Button;
+`;
+
+const output = `import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+class Button extends Component {
+  static propTypes = {
+    className: PropTypes.string
+  };
+
+  render() {
+    const { className } = this.props;
+
+    return (
+      <div className={classNames('button', className)}>
+        Button
+      </div>
+    );
+  }
+}
+
+export default Button;
+`;
+
+export default { input, output };
--- frontend/src/data/react/add-classname/index.js
+++ frontend/src/data/react/add-classname/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import example1 from './example1';
+import example2 from './example2';
 
 export default {
   id: 'add-classname',
@@ -8,5 +10,29 @@ export default {
       Adds className prop to a component and applies it for its root JSX element.
       Uses <code>classNames</code> if necessary.
     </div>
-  )
+  ),
+  actions: (
+    <ul>
+      <li>Adds <code>propTypes</code> definition to the component if not present</li>
+      <li>Adds <code>prop-types</code> import statement if not present</li>
+      <li>Adds <code>className</code> to <code>propTypes</code> definition if not present</li>
+      <li>Declares <code>className</code> in render body or functional component arguments if not present</li>
+      <li>Uses <code>className</code> on root JSX element if not present</li>
+      <li>
+        If root JSX element already has <code>className</code> defined its value will
+        be preserved by applying <code>classNames</code> function
+      </li>
+      <li>Adds <code>classnames</code> import statement if necessary</li>
+    </ul>
+  ),
+  worksWith: [ 'arrow', 'function', 'class' ],
+  dependencies: [
+    { name: 'react', type: 'always' },
+    { name: 'prop-types', type: 'always' },
+    { name: 'classnames', type: 'sometimes' }
+  ],
+  examples: [
+    example1,
+    example2
+  ]
 };
