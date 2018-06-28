8a2c3e5cfc5d08f5b4b8966850e0fa5a9c3129d7
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-28 05:28:06 +0200

    Updating description

--- frontend/src/pages/documentation/list.js
+++ frontend/src/pages/documentation/list.js
@@ -15,6 +15,48 @@ const List = () => (
   <Container>
     <Row>
       <Col md={6}>
+        <h3>What is R-Factor?</h3>
+        <div className="text-muted mb-4">
+          <p>
+            R-Factor is an automated refactoring tool for JavaScript designed
+            to help developers who work with React & Redux.
+          </p>
+          <p>
+            It features automated transformations for very common code manipulations
+            that React developers perform manually dozens of times on a daily basis.
+          </p>
+          <p>
+            If you value your time, you'll love it.
+          </p>
+          <p>
+            We know it's awesome because we use it too.
+          </p>
+          <p>
+            In fact, after we finished implementing proof of concept of the very first transformation,
+            the only thing we could say was "why didn't we do this earlier?".
+          </p>
+          <p>
+            While our initial goal was to provide React & Redux refactorings only, some cool
+            side effects appeared along the way. That is why now you can also enjoy features like
+            {' '}
+            <LinkContainer to="/documentation/sort-imports">
+              <a href="/documentation/sort-imports">Sort imports</a>
+            </LinkContainer>
+            {' or '}
+            <LinkContainer to="/documentation/sort-object-attributes">
+              <a href="/documentation/sort-object-attributes">Sort object attributes</a>
+            </LinkContainer>
+            {' .'}
+          </p>
+          <p>
+            As I am writing this we have over 30 ideas for other features, which would take months
+            to implement. And yes, some of them go beyond React & Redux. We want to see how much
+            interest will this tool get first.
+          </p>
+        </div>
+      </Col>
+
+      <Col md={6}>
         <h3>R-Factor promises to</h3>
         <ul className="text-muted mb-4">
           <li>Try its best to not break your code</li>
@@ -29,9 +71,7 @@ const List = () => (
           </li>
           <li>Prefer the fanciest, sweetest syntax possible (think ES6+)</li>
         </ul>
-      </Col>
 
-      <Col md={6}>
         <h3>User commandments</h3>
         <ul className="text-muted mb-4">
           <li>Thou shalt not refactor invalid JavaScript</li>
@@ -39,6 +79,7 @@ const List = () => (
           <li>Thou shalt not declare multiple React components in a single file</li>
           <li>Thou shalt not refactor any other foolish constructions</li>
           <li>Use the most recent JavaScript features</li>
+          <li>Commit before you refactor</li>
           <li>
             <LinkContainer to="/support">
               <a href="/support">Report your ideas & bugs</a>
@@ -66,7 +107,7 @@ const List = () => (
 
     <Row>
       <Col>
-        <h3>Usage</h3>
+        <h3>Usage instructions</h3>
         <ul className="text-muted mb-4">
           <li>Refactoring will be performed on your selection or else on an entire file</li>
           <li>Selection must be a valid JavaScript Program (e.g. <code>{'{ a: 1, b: 2 }'}</code> will not do!)</li>
@@ -74,6 +115,15 @@ const List = () => (
             You can use <a href="https://astexplorer.net/">astexplorer.net</a> with <code>babylon7</code> parser
             to validate your code before
             you <LinkContainer to="/support"><a href="/support">submit an issue</a></LinkContainer>
+          </li>
+          <li>
+            Code is refactored on your machine. If you use the plugin, no code will ever be
+            transmitted over the network. The plugin works offline. You're safe.
+          </li>
+          <li>
+            Our online <LinkContainer to="/try"><a href="/try">Try it</a></LinkContainer> tool
+            however does send your code to our server. But we do not store it. But we could have.
+            That's why you should use it for evaluation purposes with some dummy code only.
           </li>
         </ul>
       </Col>
