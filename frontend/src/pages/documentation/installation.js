70808837271fe4343193d22b2f83c6afa49e18e9
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-30 22:14:45 +0200

    Refactoring

--- frontend/src/pages/documentation/installation.js
+++ frontend/src/pages/documentation/installation.js
@@ -9,9 +9,15 @@ const Installation = () => (
     <h1>Installation</h1>
 
     <h5>Prerequisites</h5>
-    <div className="text-muted mb-4">
-      You must have Node.js <code>6.0.0</code> or later installed.
-    </div>
+    <ul className="text-muted mb-4">
+      <li>
+        You must have Node.js <code>6.0.0</code> or later installed.
+      </li>
+      <li>
+        <code>node</code> executable
+        must be available in your <code>PATH</code> environment variable.
+      </li>
+    </ul>
 
     <h5>Installation</h5>
     <div className="text-muted mb-4">
