4a4df023545293f5dd21bd276b49b405c68a90b9
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-30 18:49:53 +0200

    Scroll to top

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Container from 'reactstrap/lib/Container';
 import Navigation from 'components/navigation';
+import ScrollToTop from 'components/scroll-to-top';
 import DocumentationPage from 'pages/documentation';
 import HomePage from 'pages/home';
 import LicensePage from 'pages/license';
@@ -12,20 +13,22 @@ import 'styles/styles.css';
 
 const App = () => (
   <BrowserRouter>
-    <div>
-      <Navigation />
+    <ScrollToTop>
+      <div>
+        <Navigation />
 
-      <Container>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/documentation" component={DocumentationPage} />
-          <Route path="/license" component={LicensePage} />
-          <Route path="/purchase" component={PurchasePage} />
-          <Route path="/support" component={SupportPage} />
-          <Route path="/try" component={TryPage} />
-        </Switch>
-      </Container>
-    </div>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/documentation" component={DocumentationPage} />
+            <Route path="/license" component={LicensePage} />
+            <Route path="/purchase" component={PurchasePage} />
+            <Route path="/support" component={SupportPage} />
+            <Route path="/try" component={TryPage} />
+          </Switch>
+        </Container>
+      </div>
+    </ScrollToTop>
   </BrowserRouter>
 );
 
