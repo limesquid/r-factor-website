d63f9bf4e3bd7c230d59782fcbb1b7416d91065f
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-06-30 19:39:53 +0200

    Fixing prod build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import Clipboard from 'clipboard';
+import 'styles/bootstrap.min.css';
 import 'styles/open-iconic-bootstrap.css';
 
 // eslint-disable-next-line no-unused-vars
