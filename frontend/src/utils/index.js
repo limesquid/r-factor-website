f96de925d4a236cdae58f3955c0a6eb9bc905fe1
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-07-02 00:59:58 +0200

    Complete support with endpoint

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,5 +2,8 @@ export const formatError = (error) => {
   if (Array.isArray(error)) {
     return error.split('\n').filter(Boolean).join('\n');
   }
-  return 'Network error occurred. Check your internet connection and try again.';
+  if (String(error) === 'TypeError: Failed to fetch') {
+    return 'Network error occurred. Check your internet connection and try again.';
+  }
+  return error;
 };
