0319d6b61d5709e7ebe6ee34130c8d182654dac8
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-07-21 20:56:47 +0200

    Adding all settings boilerplate

---
+++ frontend/src/data/configuration/component-name-collision-pattern/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default {
+  id: 'component-name-collision-pattern',
+  name: 'Component name collision pattern',
+  summary: (
+    <span>
+      How should a component be named if ... TODO
+    </span>
+  ),
+  description: (
+    <p>
+      TODO
+    </p>
+  ),
+  setting: {
+    type: 'String',
+    defaultValue: '"${name}Component"'
+  }
+};
---
+++ frontend/src/data/configuration/default-hoc-component-name/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default {
+  id: 'default-hoc-component-name',
+  name: 'Default HOC component name',
+  summary: (
+    <span>
+      TODO
+    </span>
+  ),
+  description: (
+    <p>
+      TODO
+    </p>
+  ),
+  setting: {
+    type: 'String',
+    defaultValue: '"InnerComponent"'
+  }
+};
