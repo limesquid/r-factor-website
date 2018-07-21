519d88fd849842d6b8689d304b54edcfa86e861c
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-07-21 21:04:56 +0200

    Disconnect docs

--- frontend/src/data/redux/disconnect/index.js
+++ frontend/src/data/redux/disconnect/index.js
@@ -8,5 +8,23 @@ export default {
       Disconnects a component from redux store and removes
       all <code>mapStateToProps</code>, <code>mapDispatchToProps</code> & <code>mergeProps</code>.
     </span>
-  )
+  ),
+  actions: (
+    <ul className="text-muted">
+      <li>Removes <code>mapStateToProps</code> definition (if present)</li>
+      <li>Removes <code>mapDispatchToProps</code> definition (if present)</li>
+      <li>Removes <code>mergeProps</code> definition (if present)</li>
+      <li>Unwraps exported (returned, if it's a HOC) component from <code>connect</code> (if present)</li>
+      <li>Removes <code>connect</code> import statement (if present)</li>
+    </ul>
+  ),
+  worksWith: [ 'arrow', 'function', 'class', 'hoc-arrow', 'hoc-function', 'hoc-class' ],
+  dependencies: [
+    { name: 'react', type: 'always' },
+    { name: 'react-redux', type: 'always' }
+  ],
+  configuration: [],
+  examples: [
+    // TODO
+  ]
 };
