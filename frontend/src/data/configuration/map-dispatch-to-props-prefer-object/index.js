d12e63db01650feeae0d3f65ab23f8256aa5a11b
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-07-21 12:25:58 +0200

    Improving wording

--- frontend/src/data/configuration/map-dispatch-to-props-prefer-object/index.js
+++ frontend/src/data/configuration/map-dispatch-to-props-prefer-object/index.js
@@ -4,6 +4,9 @@ export default {
   id: 'map-dispatch-to-props-prefer-object',
   name: 'Prefer object mapDispatchToProps',
   summary: (
+    <span>Which variant of <code>mapDispatchToProps</code> to use.</span>
+  ),
+  desription: (
     <span>
       When <code>true</code>, <code>mapDispatchToProps</code> will be generated as an object.
       When <code>false</code>, <code>mapDispatchToProps</code> will be generated as an arrow function returning an object.
