0cfd64b3aafc09b66c0b5bf13dfef4e5ecf39d9f
ziqueq <ziqueq@gmail.com>
2018-07-28 14:53:24 +0200

    Docs updated (connect) and examples added

--- frontend/src/data/redux/connect-map-dispatch/index.js
+++ frontend/src/data/redux/connect-map-dispatch/index.js
@@ -8,6 +8,14 @@ export default {
       Connects a component to the redux store and generates <code>mapDispatchToProps</code> only.
     </span>
   ),
+  actions: (
+    <ul className="text-muted">
+      <li>Declares <code>mapDispatchToProps</code> below the component definition (if not present)</li>
+      <li>Wraps exported (returned, if it's a HOC) component with <code>connect</code> (if not present)</li>
+      <li>Uses <code>mapDispatchToProps</code> with said <code>connect</code> (replacing <code>null</code> values if present)</li>
+      <li>Adds <code>connect</code> import statement (if not present)</li>
+    </ul>
+  ),
   configuration: [
     'component-name-collision-pattern',
     'default-component-name',
