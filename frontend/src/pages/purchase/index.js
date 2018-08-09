f17bdf60433809fca09120d4a23cc435aadf803c
Yuriy Yakym <yakym.yuriy@gmail.com>
2018-08-09 04:13:31 +0200

    PriceDetails component

--- frontend/src/pages/purchase/index.js
+++ frontend/src/pages/purchase/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import Link from 'components/link';
 import Calculator from './calculator';
 import PriceDetails from './price-details';
 import './styles.css';
@@ -13,8 +12,6 @@ const PurchasePage = () => (
     <div className="row">
       <div className="col-lg-6">
         <PriceDetails />
-        Licenses are per-user, rather than per-machine, so you can enjoy R-Factor on as many computers and operating systems as you wish with your license.
-        &nbsp;<Link label="More" href="/eula" />
       </div>
       <Calculator />
     </div>
--- frontend/src/pages/purchase/price-details.js
+++ frontend/src/pages/purchase/price-details.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Link from 'components/link';
 
 const PriceDetails = () => (
-  <div className="price-details py-2">
-    <div className="price">
+  <div className="col-lg-10 card border-light py-2">
+    <div className="card-header price text-center text-info" style={{ fontSize: '4rem' }}>
       {process.env.REACT_APP_LICENSE_FEE}$
+    </div>
+    <div className="card-body text-center">
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          Licenses are per-user, rather than per-machine, so you can enjoy R-Factor
+          on as many computers and operating systems as you wish with your license.
+          &nbsp;<Link label="More" href="/eula" />
+        </li>
+        <li className="list-group-item">Support and updates included with subscription</li>
+      </ul>
     </div>
   </div>
 );
