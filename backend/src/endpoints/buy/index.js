b1dc1ac847d035410f32c7a23a0f356734110b80
Yuriy Yakym <yakym.yuriy@gmail.com>
2018-08-12 20:37:37 +0200

    BE: licenses-db && paypal acces token manager && complete payment

--- backend/src/endpoints/buy/index.js
+++ backend/src/endpoints/buy/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const { generateLicence } = require('./utils');
 const { createPayment } = require('./paypal');
+const licensesDb = require('../../utils/licenses-db');
 
 const createNewPayment = async(request, response) => {
   const payment = await createPayment();
+  const { fullName, email } = request.body;
+
+  licensesDb.create({
+    paymentId: payment.id,
+    paymentMethod: 'paypal',
+    fullName,
+    email
+  });
+
   response.send({
-    id: payment.id
+    paymentId: payment.id
   });
 };
 
-const postGenerateLicence = (request, response) => {
-  const { fullname, email } = request.body;
-  response.send(generateLicence({ fullname, email }));
+const completePayment = (request, response) => {
+  const { paymentId } = request.body;
+  const { fullName, email } = licensesDb.getByPaymentId(paymentId);
+  const licenseKey = generateLicence({ fullName, email });
+  licensesDb.setLicenseKey(paymentId, licenseKey);
+  response.send({ licenseKey });
 };
 
 module.exports = express.Router()
-  .post('/generate', postGenerateLicence)
+  .post('/complete-payment', completePayment)
   .post('/create-payment', createNewPayment);
