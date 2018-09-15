b00655292f6431f02d7f36d9db9f18301b8b7e68
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-09-15 20:36:02 +0200

    Updating usage instructions

--- frontend/src/pages/documentation/usage.js
+++ frontend/src/pages/documentation/usage.js
@@ -31,19 +31,16 @@ const Usage = () => (
         however does send your code to our server. But we do not store it. But we could have.
         That's why you should use it for evaluation purposes with some dummy code only.
       </li>
-    </ul>
-
-    <h3>Sublime Text 3</h3>
-    <div className="text-muted">
-      <p>
+      <li>
         Every refactoring is available as a command in the
         {' '}
         <a href="http://docs.sublimetext.info/en/latest/extensibility/command_palette.html">
           Command Palette
         </a>
         .
-      </p>
-      <p>
+        {/*TODO: add links to other editors*/}
+      </li>
+      <li>
         Command name corresponts to a refactoring name, but is prefixed with <code>R-Factor: </code>.
         So for example
         {' '}
@@ -52,20 +49,34 @@ const Usage = () => (
         refactoring will appear as <code>R-Factor: Convert to class component</code>.
         Remember to use fuzzy search, so that you do not have to type the whole refactoring
         name - <code>rfctcc</code> will suffice.
-      </p>
-    </div>
+      </li>
+    </ul>
 
-    <h5>Configuration</h5>
-    <div className="text-muted mb-4">
+    <h3>Configuration</h3>
+    <p className="text-muted">
+      You can use our online
+      {' '}
+      <Link href="/try" label="Try it" /> tool to generate a config file.
+    </p>
+
+    <h4>Atom</h4>
+    <p className="text-muted">
+      TODO
+    </p>
+
+    <h4>Sublime Text 3</h4>
+    <p className="text-muted">
       Open up <code>Preferences</code> <Icon name="arrow-right" /> <code>Package Settings</code>
       {' '}
       <Icon name="arrow-right" /> <code>R-Factor</code> <Icon name="arrow-right" />
       {' '}
-      <code>Settings - User</code> in Sublime menu. Whatever settings file our
-      {' '}
-      <Link href="/try" label="Try it" /> online tool generates you can paste there.
-    </div>
+      <code>Settings - User</code> in Sublime menu.
+    </p>
 
+    <h4>VSCode</h4>
+    <p className="text-muted">
+      TODO
+    </p>
   </Fragment>
 );
 
