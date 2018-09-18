6a0eb0d12f9c3dc3b6b99f05fe1c1135bf98016d
Yuriy Yakym <yakym.yuriy@gmail.com>
2018-09-18 03:23:26 +0300

    Payu create payment integration

--- backend/src/endpoints/buy/constants.js
+++ backend/src/endpoints/buy/constants.js
@@ -1,17 +1,10 @@
-const BAD_ADDRESS_ERROR_MESSAGE = 'Bad address';
-const BAD_COMPANY_NAME = 'Bad company name';
-const BAD_EMAIL_ERROR_MESSAGE = 'Bad email address';
-const BAD_FULL_NAME_ERROR_MESSAGE = 'Bad full name';
-const BAD_VATIN = 'Bad NIP/VATIN';
-const GENERATE_INVOICE_ERROR_MESSAGE = 'Error while generating invoice document';
-const PAYMENT_CREATION_ERROR_MESSAGE = 'Error while creating payment';
-
 module.exports = {
-  BAD_ADDRESS_ERROR_MESSAGE,
-  BAD_COMPANY_NAME,
-  BAD_EMAIL_ERROR_MESSAGE,
-  BAD_FULL_NAME_ERROR_MESSAGE,
-  BAD_VATIN,
-  GENERATE_INVOICE_ERROR_MESSAGE,
-  PAYMENT_CREATION_ERROR_MESSAGE
+  BAD_ADDRESS_ERROR_MESSAGE: 'Bad address',
+  BAD_COMPANY_NAME: 'Bad company name',
+  BAD_EMAIL_ERROR_MESSAGE: 'Bad email address',
+  BAD_FULL_NAME_ERROR_MESSAGE: 'Bad full name',
+  BAD_VATIN: 'Bad NIP/VATIN',
+  GENERATE_INVOICE_ERROR_MESSAGE: 'Error while generating invoice document',
+  PAYMENT_CREATION_ERROR_MESSAGE: 'Error while creating payment',
+  WRONG_PAYMENT_ID_ERROR_MESSAGE: 'Wrong payment id'
 };
