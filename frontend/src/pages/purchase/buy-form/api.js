cd4ab084d74af9c0bacd0e38d32fdb5a4d46e154
Yuriy Yakym <yakym.yuriy@gmail.com>
2018-09-19 03:20:46 +0300

    [Frontend] linting errors

--- frontend/src/pages/purchase/buy-form/api.js
+++ frontend/src/pages/purchase/buy-form/api.js
@@ -2,7 +2,13 @@ import fetch from 'utils/fetch';
 
 const BUY_API_URL = `${process.env.REACT_APP_API_URL}/buy`;
 
-export const createPayment = ({ address, companyName, email, fullName, vatin }) => fetch(`${BUY_API_URL}/create-payment`, {
+export const createPayment = ({
+  address,
+  companyName,
+  email,
+  fullName,
+  vatin
+}) => fetch(`${BUY_API_URL}/create-payment`, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
