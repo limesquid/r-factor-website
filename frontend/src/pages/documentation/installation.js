3574915a0d5be8ef2f73e1dfef5a822650175483
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-10-14 16:53:26 +0200

    Add link to download page in documentation

--- frontend/src/pages/documentation/installation.js
+++ frontend/src/pages/documentation/installation.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
+import Link from 'components/link';
 
 const Installation = () => (
   <Fragment>
@@ -11,14 +12,23 @@ const Installation = () => (
     <h5>Prerequisites</h5>
     <ul className="text-muted mb-4">
       <li>
-        You must have Node.js <code>6.0.0</code> or later installed.
+        You must have <a href="https://nodejs.org/en/download/">Node.js</a> <code>6.0.0</code> or later installed.
+        The newer the better.
+      </li>
+      <li>
+        You must have R-Factor <Link href="/purchase" label="license key" />.
       </li>
     </ul>
 
-    <h5>Installation</h5>
-    <div className="text-muted mb-4">
-      TODO
-    </div>
+    <h5>Instructions</h5>
+    <ol className="text-muted mb-4">
+      <li>
+        Download R-Factor plugin for your editor from our <Link href="/download" label="download page" />.
+      </li>
+      <li>
+        TODO
+      </li>
+    </ol>
   </Fragment>
 );
 
