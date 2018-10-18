c85e369e52998d477997355ce8be064ee2b4cf7d
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-10-18 02:30:24 +0200

    Change offsets on purchase page

--- frontend/src/pages/purchase/index.js
+++ frontend/src/pages/purchase/index.js
@@ -11,10 +11,10 @@ const PurchasePage = () => (
       <title>R-Factor - Purchase</title>
     </Helmet>
     <Row>
-      <Col md={6} xl={{ size: 5, offset: 1 }} className="mb-4">
+      <Col md={6} xl={5} className="mb-4">
         <PaymentPanel />
       </Col>
-      <Col md={6} xl={{ size: 5, offset: 1 }} className="mb-4">
+      <Col md={6} xl={{ size: 5, offset: 2 }} className="mb-4">
         <Calculator />
       </Col>
     </Row>
