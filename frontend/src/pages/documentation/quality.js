ab3db73fa5647a91773bcb141df8df720b02970c
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-10-19 12:53:40 +0200

    Wording in quality page

--- frontend/src/pages/documentation/quality.js
+++ frontend/src/pages/documentation/quality.js
@@ -10,21 +10,20 @@ const Quality = () => (
       <p>
         R-Factor has been thoroughly tested. We have <span className="text-body">100%</span> test coverage.
         Every statement, every branch, every function & every line is covered.
+        {' '}<span className="text-body">833</span> test cases, all <span className="text-success">green</span>!
         We have ran this tool on over <span className="text-body">20</span> different
         projects with different settings. We've gathered feedback from a few idependent developers.
-        It's still just a software, but we feel we did our best.
+        It's still just software, but we feel we did pretty damn good.
       </p>
       <p>
         It's worth noting that not once have we experienced performance issues.
       </p>
       <img
+        className="my-2"
         src={randyUrl}
         alt={IMAGE_DESCRIPTION}
         title={IMAGE_DESCRIPTION}
-        style={{
-          width: '100%',
-          margin: '8px 0'
-        }} />
+        style={{ width: '100%' }} />
       <p className="text-center">{IMAGE_DESCRIPTION}</p>
     </div>
   </Fragment>
