a0b2ffd79705c5e9d2172a3589460afa502180cd
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-11-06 23:32:43 +0700

    Update privacy policy styling

--- frontend/src/pages/privacy-policy/introduction.js
+++ frontend/src/pages/privacy-policy/introduction.js
@@ -2,12 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Introduction = ({ className }) => (
-  <div className={className}>
-    <p>
-      If you have any questions regarding our Privacy Policy remember that you
-      can contact us at any time by writing us a message using our contact form.
-    </p>
-  </div>
+  <p className="text-muted">
+    If you have any questions regarding our Privacy Policy remember that you
+    can contact us at any time by writing us a message using our contact form.
+  </p>
 );
 
 Introduction.propTypes = {
