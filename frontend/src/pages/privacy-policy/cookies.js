e43b8b150cd8bde03f1efc96f89e2acb1a68bd8e
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-11-06 23:32:43 +0700

    Update privacy policy styling

--- frontend/src/pages/privacy-policy/cookies.js
+++ frontend/src/pages/privacy-policy/cookies.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'components/link';
 
 const Cookies = ({ className }) => (
-  <div className={className}>
+  <Fragment>
     <h3>Cookies and tracking technologies</h3>
-    <p>
+    <p className="text-muted">
       Our website, like almost all other websites, uses cookies to provide you with the best browsing experience.
     </p>
-    <p>
+    <p className="text-muted">
       Cookies are small pieces of text information stored on your end device
       (computer, tablet, smartphone) that can be read by our Information and Communications Technology system.
     </p>
-    <p>
+    <p className="text-muted">
       Cookies allow us to:
     </p>
-    <ol>
+    <ol className="text-muted">
       <li>
         use analytical tools
       </li>
@@ -23,12 +23,12 @@ const Cookies = ({ className }) => (
         preserve your settings
       </li>
     </ol>
-    <p>
+    <p className="text-muted">
       More details can be found below.
     </p>
 
-    <h5 className="mt-4">Agreeing to cookies</h5>
-    <p>
+    <h5>Agreeing to cookies</h5>
+    <p className="text-muted">
       When you visit the website for the first time, you are shown a message about the use of cookies.
       Accepting and closing this message means that you agree to the use of cookies in accordance with
       the provisions of this privacy policy. You can always withdraw your consent by deleting cookies
@@ -36,21 +36,21 @@ const Cookies = ({ className }) => (
       difficulties in using our website as well as many other websites that use cookies.
     </p>
 
-    <h5 className="mt-4">Third-party cookies</h5>
-    <p>
+    <h5>Third-party cookies</h5>
+    <p className="text-muted">
       Our website, like most of today's websites, uses functions provided by third parties which
       involve the use of third-party cookies. The use of these types of files is described below.
     </p>
 
-    <h5 className="mt-4">Analysis and statistics</h5>
-    <p>
+    <h5>Analysis and statistics</h5>
+    <p className="text-muted">
       This site uses the <Link label="Yandex.Metrica" href="https://yandex.com/support/metrica/index.html" /> web
       analytics service provided by the company Yandex Oy Limited Company - Moreenikatu 6, 04600 Mantsala, Finland.
     </p>
-    <p>
+    <p className="text-muted">
       Yandex.Metrica uses “cookies” small text files placed on user’s computers to analyze user activity.
     </p>
-    <p>
+    <p className="text-muted">
       Information collected by cookies does not reveal your identity, but it can help us to improve our website
       performance. Information about your use of this website collected by cookies will be transferred to Yandex
       and stored on Yandex’s server in the EU and the Russian Federation. Yandex will process this information to
@@ -58,20 +58,20 @@ const Cookies = ({ className }) => (
       Yandex processes this information as specified in
       the <Link label="Terms of Use" href="https://yandex.com/legal/metrica_termsofuse/" /> of Yandex.Metrica Service.
     </p>
-    <p>
+    <p className="text-muted">
       You can stop using cookies be making relevant adjustments in browser settings.
       You can also use the <Link label="tool" href="https://yandex.com/support/metrika/general/opt-out.html" />.
       However, it can affect some website functions. By using this website, you agree that
       Yandex can process your data in the above manner and for the above purposes.
     </p>
 
-    <h5 className="mt-4">Payment system</h5>
-    <p>
+    <h5>Payment system</h5>
+    <p className="text-muted">
       We use <Link label="PayU" href="https://www.payu.pl/en" /> payment system.
       PayU is not integrated with the Website.
       The payment is made entirely via PayU service.
     </p>
-  </div>
+  </Fragment>
 );
 
 Cookies.propTypes = {
--- frontend/src/pages/privacy-policy/short-version.js
+++ frontend/src/pages/privacy-policy/short-version.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'components/link';
 
 const ShortVersion = ({ className }) => (
-  <div className={className}>
+  <Fragment>
     <h3>Short version – key information</h3>
-    <p>
+    <p className="text-muted">
       We care about your privacy and value your time. That is why we have prepared
       a short version of the most important rules related to privacy protection.
     </p>
 
-    <ul>
+    <ul className="text-muted">
       <li>
         We use the <Link label="Yandex.Metrica" href="https://yandex.com/support/metrica/index.html" /> web analytics
         service provided by the company Yandex Oy Limited Company - Moreenikatu 6, 04600 Mantsala, Finland,
@@ -21,10 +21,10 @@ const ShortVersion = ({ className }) => (
       </li>
     </ul>
 
-    <p>
+    <p className="text-muted">
       If the above information is not enough for you, you will find further details below.
     </p>
-  </div>
+  </Fragment>
 );
 
 ShortVersion.propTypes = {
