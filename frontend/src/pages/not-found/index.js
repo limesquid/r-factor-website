df1cb523e2776eb7727a40a938f99ae6c2874d22
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-11-10 00:58:37 +0700

    Update NotFound page

--- frontend/src/pages/not-found/index.js
+++ frontend/src/pages/not-found/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Link from 'components/link';
 
-const PurchasePage = () => (
+const NotFound = () => (
   <div>
     <Helmet>
       <title>R-Factor - 404 - Not found</title>
@@ -11,10 +11,10 @@ const PurchasePage = () => (
     <h1><span className="text-danger">404</span> - Not found</h1>
     <h4 className="mb-4">We don't know what you're looking for, but it's not here.</h4>
     <p>
-      If you think this is an issue and <code>{window.location.pathname}</code> is a valid URL,
+      If you think this is an issue and <code>{window.location.href}</code> is a valid URL,
       please report it <Link href="/support" label="here" />.
     </p>
   </div>
 );
 
-export default PurchasePage;
+export default NotFound;
