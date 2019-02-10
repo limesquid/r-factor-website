a518e68407e377edfc7463960ab95295e8e0a7e1
Yuriy Yakym <yakym.yuriy@gmail.com>
2019-02-10 18:33:28 +0200

    [Backend] linter fixes

--- backend/src/endpoints/buy/index.js
+++ backend/src/endpoints/buy/index.js
@@ -1,10 +1,7 @@
 /* eslint-disable max-statements, max-depth, max-len */
 const express = require('express');
-const moment = require('moment-business-days');
 const completePayment = require('./complete-payment');
 const createPayment = require('./create-payment');
-const generateInvoicePdf = require('./generate-invoice-pdf');
-const { getUsdRate } = require('../../utils/currency-rates');
 
 module.exports = express.Router()
   .post('/complete-payment', completePayment)
