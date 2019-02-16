99a590ae83c5c3969d4759cb5e6a5741cd6fe67f
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2019-02-16 18:55:39 +0700

    Fix snap

--- frontend/src/pages/not-found/index.js
+++ frontend/src/pages/not-found/index.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
+import Col from 'reactstrap/lib/Col';
+import Row from 'reactstrap/lib/Row';
 import Link from 'components/link';
 
 const NotFound = () => (
@@ -8,12 +10,20 @@ const NotFound = () => (
       <title>R-Factor - 404 - Not found</title>
     </Helmet>
 
-    <h1><span className="text-danger">404</span> - Not found</h1>
-    <h4 className="mb-4">We don't know what you're looking for, but it's not here.</h4>
-    <p>
-      If you think this is an issue and <code>{window.location.href}</code> is a valid URL,
-      please report it <Link href="/support" label="here" />.
-    </p>
+    <Row>
+      <Col>
+        <h1>
+          <span className="text-danger">404</span> - Not found
+        </h1>
+        <h4 className="mb-4">
+          We don't know what you're looking for, but it's not here.
+        </h4>
+        <p>
+          If you think this is an issue and <code>{window.location.href}</code>{' '}
+          is a valid URL, please report it <Link href="/support" label="here" />.
+        </p>
+      </Col>
+    </Row>
   </Fragment>
 );
 
